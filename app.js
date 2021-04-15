23c290df612b4b6ebd8e05437ff52f1d9d9918f0
Laviel <fafansin@gmail.com>
2021-04-15 11:11:35 +0300

    Add env file and basic mongoose connection

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const dotenv = require('dotenv');
 const methodOverride = require('method-override');
 const mongoose = require('mongoose');
+
+dotenv.config();
+
+mongoose.connect(process.env.DB_HOST, {
+        useFindAndModify: false,
+        useUnifiedTopology: true,
+        useNewUrlParser: true
+    })
+    .then(()=>{
+        console.log('Mongodb Connection Success');
+    })
+    .catch(e=>{
+        console.log('Mongodb Connection Failed');
+        console.log(e);
+    })
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -18,6 +34,6 @@ app.get('*', (req, res) =>{
     res.render('404');
 })
 
-app.listen(3000, ()=>{
-    console.log('Application loaded at port 3000');
+app.listen(process.env.PORT, ()=>{
+    console.log(`Application loaded at port ${process.env.PORT}` );
 })
